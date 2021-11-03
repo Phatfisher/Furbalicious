73f6cf52e1c87f46c107cb40373b85e523dc8ee0
Ditronian <davidgereau@gmail.com>
2021-11-03 14:36:03 -0600

    Completed Database Models
    - Implemented models.py
    - Implemented admin.py
    
    We can now manually make Furbies...

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Furby, Order, CustomUser, OrderFurbies
 
-# Register your models here.
+class FurbyAdmin(admin.ModelAdmin):
+    model = Furby
+
+class OrderAdmin(admin.ModelAdmin):
+    model = Order
+
+class CustomUserAdmin(admin.ModelAdmin):
+    model = CustomUser
+
+class OrderFurbiesAdmin(admin.ModelAdmin):
+    model = OrderFurbies
+
+admin.site.register(Furby)
+admin.site.register(Order)
+admin.site.register(CustomUser)
+admin.site.register(OrderFurbies)
