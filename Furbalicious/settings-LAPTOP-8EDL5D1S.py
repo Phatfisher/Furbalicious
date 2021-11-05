1cdca2382b5dac656c64c17662a9df57bd8ebf2b
Ditronian <davidgereau@gmail.com>
2021-11-05 13:57:46 -0600

    Herokufied Project
    Added heroku stuff.

--- Furbalicious/settings-LAPTOP-8EDL5D1S.py
+++ Furbalicious/settings-LAPTOP-8EDL5D1S.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'qwxb3oidsp$bg)4&l_c=b0l4cv@648nik^$3t-zb_ppr8lw@9l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
