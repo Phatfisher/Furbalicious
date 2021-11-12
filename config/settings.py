38b5979d107a6c9b8529d0fc14d28d0e6fe1c83f
Ditronian <davidgereau@gmail.com>
2021-11-12 12:33:28 -0700

    Changes Settings Directory Name to config
    - Hopefully heroku is now happy.

--- config/settings.py
+++ config/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'Furbalicious.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'Furbalicious.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 #Email System
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
